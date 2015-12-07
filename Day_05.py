23864fc76c30061a3d6a06d3919445ab19d0f9f9
Karl Melcher <kmelcher@gmail.com>
2015-12-07 08:49:15 -0500

    work on day5

--- Day_05.py
+++ Day_05.py
@@ -5,7 +5,7 @@ import re
 # Day 5  Naughty or Nice?
 
 
-def isNice(line):
+def isNice5a(line):
     # at least 3 vowels
     m = re.match(".*([aeiou]).*([aeiou]).*([aeiou]).*", line)
     if m is None:
@@ -35,7 +35,7 @@ def day5a(filename):
         niceCount = 0
         lines = infile.readlines()
         for line in lines:
-            if isNice(line):
+            if isNice5a(line):
                 print "%s %s" % (line, "Nice")
                 niceCount += 1
             else:
@@ -43,5 +43,52 @@ def day5a(filename):
 
         print "Nice Count = %d" % niceCount
 
+# ----------------------------------------
 
-day5a("Day_05a.input")  # answer: 236
+
+def isNice5b(line):
+    # pair of letters duplicated, but not overlapping
+    #  abxab OK, aaa not ok
+
+    # 0 based indexing
+
+    print ("[%s] len=%d" % (line, len(line)))
+    match = 0
+    for firstPairStart in range(0, len(line)-3):
+        testPat = line[firstPairStart]+line[firstPairStart+1]
+        if line.count(testPat) >= 2:
+            match += 1
+    if match == 0:
+        return False
+
+    # matching letter
+    #  xyx
+
+    for i in range(0, len(line)-3):
+        if line[i] == line[i+2]:
+            return True
+
+    return False
+
+
+def day5b(filename):
+    with open(filename, 'r') as infile:
+        niceCount = 0
+        naughtyCount = 0
+        lines = infile.readlines()
+        for line in lines:
+            if isNice5b(line.strip()):
+                print "%s %s" % (line, "Nice")
+                niceCount += 1
+            else:
+                print "%s %s" % (line, "naughty")
+                naughtyCount += 1
+        print "Nice Count = %d" % niceCount
+        print "Naughty Count = %d" % naughtyCount
+
+
+#day5a("Day_05a.input")  # answer: 236
+#day5b("Day_05b.test.input")  # answer: 236
+day5b("Day_05a.input")  # answer: 236
+
+# 49 too low
