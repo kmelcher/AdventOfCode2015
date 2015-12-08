297b29e760ca8ab504322252a5bdcb8116b8c4c0
Karl Melcher <kmelcher@gmail.com>
2015-12-08 12:18:44 -0500

    day 7 WIP

--- Day_07a.py
+++ Day_07a.py
@@ -15,6 +15,21 @@ def initCircuit():
     circuit = {}
 
 
+# values in can either be a reference to a signal
+# or a number (signal).  If alpha (a-z), then deref (if it exists)
+# and return value.
+def getValue(strIn):
+    global circuit
+    v = 0
+    m = re.match("[0-9]{1,}", strIn)
+    if m is not None:
+        v = int(strIn)
+    else:
+        if strIn in circuit:
+            v = circuit[strIn]
+    return v
+
+
 def day7a(filename):
     global circuit
     initCircuit()
@@ -29,13 +44,12 @@ def day7a(filename):
 
             if "AND" in line:
                 print "and"
-                m = re.match("([a-z]{0,}) AND ([a-z]{0,}) -> ([a-z]{0,}).*", line.strip())
+                m = re.match("([a-z0-9]{0,}) AND ([a-z0-9]{0,}) -> ([a-z]{0,}).*", line.strip())
                 print m.groups()
                 if m is None or len(m.groups()) != 3:
                     print "input error line %d %s" % (lineNum, line)
                     return
-                if m.group(1) in circuit and m.group(2) in circuit:      
-                    circuit[m.group(3)] = circuit[m.group(1)] & circuit[m.group(2)]
+                circuit[m.group(3)] = getValue(m.group(1)) & getValue(m.group(2))
             elif "OR" in line:
                 print "or"
                 m = re.match("([a-z]{0,}) OR ([a-z]{0,}) -> ([a-z]{0,}).*", line.strip())
@@ -43,8 +57,7 @@ def day7a(filename):
                 if m is None or len(m.groups()) != 3:
                     print "input error line %d %s" % (lineNum, line)
                     return
-                if m.group(1) in circuit and m.group(2) in circuit:      
-                    circuit[m.group(3)] = circuit[m.group(1)] | circuit[m.group(2)]
+                circuit[m.group(3)] = getValue(m.group(1)) | getValue(m.group(2))
             elif "LSHIFT" in line:
                 print "ls"
                 m = re.match("([a-z]{0,}) LSHIFT ([0-9]{0,}) -> ([a-z]{0,}).*", line.strip())
@@ -52,8 +65,7 @@ def day7a(filename):
                 if m is None or len(m.groups()) != 3:
                     print "input error line %d %s" % (lineNum, line)
                     return
-                if m.group(1) in circuit:      
-                    circuit[m.group(3)] = circuit[m.group(1)] << int(m.group(2))
+                circuit[m.group(3)] = getValue(m.group(1)) << getValue(m.group(2))
             elif "RSHIFT" in line:
                 print "ls"
                 m = re.match("([a-z]{0,}) RSHIFT ([0-9]{0,}) -> ([a-z]{0,}).*", line.strip())
@@ -61,8 +73,7 @@ def day7a(filename):
                 if m is None or len(m.groups()) != 3:
                     print "input error line %d %s" % (lineNum, line)
                     return
-                if m.group(1) in circuit:      
-                    circuit[m.group(3)] = circuit[m.group(1)] >> int(m.group(2))
+                circuit[m.group(3)] = getValue(m.group(1)) >> getValue(m.group(2))
             elif "NOT" in line:
                 print "not"
                 m = re.match("NOT ([a-z]{0,}) -> ([a-z]{0,}).*", line.strip())
@@ -70,18 +81,20 @@ def day7a(filename):
                 if m is None or len(m.groups()) != 2:
                     print "input error line %d %s" % (lineNum, line)
                     return
-                if m.group(1) in circuit:      
-                    circuit[m.group(2)] = (~circuit[m.group(1)] & 0xFFFF)
+                circuit[m.group(2)] = (~getValue(m.group(1)) & 0xFFFF)
             else:
                 print "assign"
-                m = re.match("([0-9]{0,}) -> ([a-z]{0,}).*", line.strip())
+                m = re.match("([a-z0-9]{0,}) -> ([a-z]{0,}).*", line.strip())
                 print m.groups()
                 if m is None or len(m.groups()) != 2:
                     print "input error line %d %s" % (lineNum, line)
                     return 
-                circuit[m.group(2)] = int(m.group(1))
+                circuit[m.group(2)] = getValue(m.group(1))
 
     print circuit
+
+    print "a = %d" % circuit['a']
+
 #123 -> x
 #456 -> y
 #x AND y -> d
@@ -92,7 +105,8 @@ def day7a(filename):
 #NOT y -> i
 
 
-day7a("Day_07.input")
-# day6a("Day_06.test.input")
+day7a("Day_07.test.input")
+# day7a("Day_07.input")
+
 
 # test()
